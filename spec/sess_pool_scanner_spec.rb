ae923827c922710904ba82a701672cbe65cf32b4
Matt Weppler <mweppler@gmail.com>
2013-04-17 19:31:43 -0700

    rewrote specs to use expect instead of should

--- spec/sess_pool_scanner_spec.rb
+++ spec/sess_pool_scanner_spec.rb
@@ -8,88 +8,69 @@ describe SessPoolScanner do
 /* This is a comment */
 CODE
     result = scanner.tokenize code
-    result[0][0].should == :COMMENT_BEGIN
-    result[0][1].should == "/*"
-    result[1][0].should == :COMMENT
-    result[1][1].should == " This is a comment "
-    result[2][0].should == :COMMENT_END
-    result[2][1].should == "*/"
+    expect(result[0]).to eq([:COMMENT_BEGIN, "/*"])
+    expect(result[1]).to eq([:COMMENT, " This is a comment "])
+    expect(result[2]).to eq([:COMMENT_END, "*/"])
   end
 
   it 'tokenizes the following keywords (if, else, true, false, nil)' do
     result = scanner.tokenize("if else true false nil")
-    result[0][0].should == :IF
-    result[0][1].should == "if"
-    result[1][0].should == :ELSE
-    result[1][1].should == "else"
-    result[2][0].should == :TRUE
-    result[2][1].should == "true"
-    result[3][0].should == :FALSE
-    result[3][1].should == "false"
-    result[4][0].should == :NIL
-    result[4][1].should == "nil"
+    expect(result[0]).to eq([:IF, "if"])
+    expect(result[1]).to eq([:ELSE, "else"])
+    expect(result[2]).to eq([:TRUE, "true"])
+    expect(result[3]).to eq([:FALSE, "false"])
+    expect(result[4]).to eq([:NIL, "nil"])
   end
 
   it 'tokenizes an identifier' do
     result = scanner.tokenize("print")
-    result[0][0].should == :IDENTIFIER
-    result[0][1].should == "print"
+    expect(result[0]).to eq([:IDENTIFIER, "print"])
   end
 
   it 'tokenizes an identifier even if a keyword (if) is within the text of the identifier' do
     result = scanner.tokenize("identifier")
-    result[0][0].should == :IDENTIFIER
-    result[0][1].should == "identifier"
+    expect(result[0]).to eq([:IDENTIFIER, "identifier"])
   end
 
   it 'tokenizes a constant' do
     result = scanner.tokenize("MY_CONSTANT")
-    result[0][0].should == :CONSTANT
-    result[0][1].should == "MY_CONSTANT"
+    expect(result[0]).to eq([:CONSTANT, "MY_CONSTANT"])
   end
 
   it 'tokenizes a float' do
     result = scanner.tokenize("30.00")
-    result[0][0].should == :FLOAT
-    result[0][1].should == 30.0
+    expect(result[0]).to eq([:FLOAT, 30.0])
   end
 
   it 'tokenizes a negative float' do
     result = scanner.tokenize("-25.0")
-    result[0][0].should == :FLOAT
-    result[0][1].should == -25.0
+    expect(result[0]).to eq([:FLOAT, -25.0])
   end
 
   it 'tokenizes an integer' do
     result = scanner.tokenize("3000")
-    result[0][0].should == :INTEGER
-    result[0][1].should == 3000
+    expect(result[0]).to eq([:INTEGER, 3000])
   end
 
   it 'tokenizes a negative integer' do
     result = scanner.tokenize("-5")
-    result[0][0].should == :INTEGER
-    result[0][1].should == -5
+    expect(result[0]).to eq([:INTEGER, -5])
   end
 
   it 'tokenizes a symbol' do
     result = scanner.tokenize(":PAGE")
-    result[0][0].should == :SYMBOL
-    result[0][1].should == ":PAGE"
+    expect(result[0]).to eq([:SYMBOL, ":PAGE"])
   end
 
   it 'tokenizes a string' do
     result = scanner.tokenize("\"hello\"")
-    result[0][0].should == :STRING
-    result[0][1].should == "\"hello\""
+    expect(result[0]).to eq([:STRING, "\"hello\""])
   end
 
   it 'tokenizes a number and a string' do
     result = scanner.tokenize("\"hello\" 3000")
-    result[0][0].should == :STRING
-    result[0][1].should == "\"hello\""
-    result[1][0].should == :INTEGER
-    result[1][1].should == 3000
+    expect(result[0]).to eq([:STRING, "\"hello\""])
+    expect(result[1]).to eq([:INTEGER, 3000])
   end
 
   it 'tokenizes a simple if/else block' do
@@ -100,44 +81,27 @@ else:
   print "false"
 CODE
     result = scanner.tokenize(code)
-    result[0][0].should == :IF
-    result[0][1].should == "if"
-    result[1][0].should == :IDENTIFIER
-    result[1][1].should == "test"
-    result[2][0].should == :OPERATOR
-    result[2][1].should == "=="
-    result[3][0].should == :STRING
-    result[3][1].should == "\"test\""
-    result[4][0].should == :VALUE
-    result[4][1].should == ":"
-    result[5][0].should == :INDENT
-    result[5][1].should == 2
-    result[6][0].should == :IDENTIFIER
-    result[6][1].should == "print"
-    result[7][0].should == :STRING
-    result[7][1].should == "\"true\""
-    result[8][0].should == :OUTDENT
-    result[8][1].should == 0
-    result[9][0].should == :ELSE
-    result[9][1].should == "else"
-    result[10][0].should == :VALUE
-    result[10][1].should == ":"
-    result[11][0].should == :INDENT
-    result[11][1].should == 2
-    result[12][0].should == :IDENTIFIER
-    result[12][1].should == "print"
-    result[13][0].should == :STRING
-    result[13][1].should == "\"false\""
+    expect(result[0]).to eq([:IF, "if"])
+    expect(result[1]).to eq([:IDENTIFIER, "test"])
+    expect(result[2]).to eq([:OPERATOR, "=="])
+    expect(result[3]).to eq([:STRING, "\"test\""])
+    expect(result[4]).to eq([:VALUE, ":"])
+    expect(result[5]).to eq([:INDENT, 2])
+    expect(result[6]).to eq([:IDENTIFIER, "print"])
+    expect(result[7]).to eq([:STRING, "\"true\""])
+    expect(result[8]).to eq([:OUTDENT, 0])
+    expect(result[9]).to eq([:ELSE, "else"])
+    expect(result[10]).to eq([:VALUE, ":"])
+    expect(result[11]).to eq([:INDENT, 2])
+    expect(result[12]).to eq([:IDENTIFIER, "print"])
+    expect(result[13]).to eq([:STRING, "\"false\""])
   end
 
   it 'tokenizes a calculation' do
     result = scanner.tokenize("5 + 20")
-    result[0][0].should == :INTEGER
-    result[0][1].should == 5
-    result[1][0].should == :OPERATOR
-    result[1][1].should == "+"
-    result[2][0].should == :INTEGER
-    result[2][1].should == 20
+    expect(result[0]).to eq([:INTEGER, 5])
+    expect(result[1]).to eq([:OPERATOR, "+"])
+    expect(result[2]).to eq([:INTEGER, 20])
  end
 
   it 'tokenizes an identifier and ignores the newline' do
@@ -146,8 +110,7 @@ print
 
 CODE
     result = scanner.tokenize(code)
-    result[0][0].should == :IDENTIFIER
-    result[0][1].should == "print"
+    expect(result[0]).to eq([:IDENTIFIER, "print"])
   end
 
   it 'tokenizes a simple function block' do
@@ -157,23 +120,14 @@ function my_function:
     print "true"
 CODE
     result = scanner.tokenize(code)
-    result[0][0].should == :FUNCTION
-    result[0][1].should == "function"
-    result[1][0].should == :IDENTIFIER
-    result[1][1].should == "my_function"
-    result[2][0].should == :VALUE
-    result[2][1].should == ":"
-    result[3][0].should == :INDENT
-    result[3][1].should == 2
-    result[4][0].should == :IF
-    result[4][1].should == "if"
-    result[5][0].should == :TRUE
-    result[5][1].should == "true"
-    result[6][0].should == :INDENT
-    result[6][1].should == 4
-    result[7][0].should == :IDENTIFIER
-    result[7][1].should == "print"
-    result[8][0].should == :STRING
-    result[8][1].should == "\"true\""
+    expect(result[0]).to eq([:FUNCTION, "function"])
+    expect(result[1]).to eq([:IDENTIFIER, "my_function"])
+    expect(result[2]).to eq([:VALUE, ":"])
+    expect(result[3]).to eq([:INDENT, 2])
+    expect(result[4]).to eq([:IF, "if"])
+    expect(result[5]).to eq([:TRUE, "true"])
+    expect(result[6]).to eq([:INDENT, 4])
+    expect(result[7]).to eq([:IDENTIFIER, "print"])
+    expect(result[8]).to eq([:STRING, "\"true\""])
   end
 end
